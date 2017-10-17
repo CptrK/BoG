c02bf4fa89d776c1c947ff07893dc2aff7ffea95
Robin Konopka <robin.konopka@itelligence.de>
2017-10-17 08:33:35 +0200

    add 'Hello World' program

---
+++ Projects/src_smallProjects/HelloWorld/Hello.java
@@ -0,0 +1,9 @@
+package HelloWorld;
+
+public class Hello {
+	
+	public static void main (String [] args) { // main-Methode
+
+		System.out.println ("Hello World"); // Ausgabe eines Textes "Hello World."
+	}
+}
