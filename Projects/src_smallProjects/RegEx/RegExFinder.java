229ce1002d619f706b3e670fae269ba8b0a80325
Robin Konopka <robin.konopka@itelligence.de>
2017-11-03 13:08:45 +0100

    fixed issues with the program, added understandable console messages to it with some detailed information for the user

--- Projects/src_smallProjects/RegEx/RegExFinder.java
+++ Projects/src_smallProjects/RegEx/RegExFinder.java
@@ -1,32 +1,56 @@
 package RegEx;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExFinder {
 	
-	String regex;
-	String content;
+	private String regex;
+	private String path;
+	public int occurs;
 
-	RegExFinder (String regex, String path) throws IOException {
-		
+	RegExFinder (String regex, String path) {
 		this.regex = regex;
-		BufferedReader br = new BufferedReader(new FileReader(path));
-		content = br.readLine();
-		br.close();
+		this.path = path;
 	}
 	
-	public void search() {
-		System.out.println(content);
-		/*Pattern p = Pattern.compile(regex);
-	    Matcher m = p.matcher(content);
-	    m.find();
-	    System.out.println(m.group());*/
-		
+	public void search() throws IOException {
+		int count=0;
+		String foundSubStr ="";
+		//reading the input file
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		//writing read content in variable
+		String temp;
+		while((temp = br.readLine()) != null) {
+			//set RegEx pattern and file that is searched in
+			Pattern p = Pattern.compile(regex);
+		    Matcher m = p.matcher(temp);
+		    //search for substrings and writing them in variable
+		    while(m.find()) {
+		    	//remove leading and ending white spaces or additional lead string
+		    	String temp2 = m.group();
+		    	if(temp2.startsWith(" ") || temp2.endsWith(" ") || temp2.startsWith("Telefon") || temp2.startsWith("WWW")) {
+		    		temp2 = temp2.replaceAll("^\\ |Telefon\\ |WWW\\ |\\ $", "");
+		    	}
+		    	foundSubStr += temp2 + "\n";
+		    	count++;
+		    }
+		}
+		br.close();
+	    //give information or the founded substrings out on console
+	    if (count > 0) {
+	    	System.out.println(foundSubStr);
+	    	System.out.println("There were " + count + " matches found for your requested regular expression.\n");
+	    } else {
+	    	System.out.println("Expected some but got no matches? Please check if you chose the right input file.\n");
+	    }
+		this.occurs = count;
+	}
+	
+	public int getOccurs() {
+		return this.occurs;
 	}
 }
--- Projects/src_smallProjects/RegEx/RegExTest.java
+++ Projects/src_smallProjects/RegEx/RegExTest.java
@@ -7,33 +7,49 @@ public class RegExTest {
 	private static String path = "D:/git/SchoolProjects/Projects/src_smallProjects/RegEx/example.txt";
 	
 	public static void main (String[] args) throws IOException {
-		//write start comment
+		//write starting comment
 		System.out.println("Start program... \n\n");
 		
-		//creating various RegExFinder
-		RegExFinder regDate = new RegExFinder("^(\\d{1,2}\\.){2}(\\20\\d{2}|\\d{2})$", path);
-		RegExFinder regPostal = new RegExFinder("^(\\d{5})$", path);
-		RegExFinder regTel = new RegExFinder("^(\\+\\d{2}\\ )(\\d{1,4}\\ )(.{4,8})$", path);
-		RegExFinder regInet = new RegExFinder("^(w{3}\\.)(\\S*\\.)(\\.{2,})$", path);
-		RegExFinder regMail = new RegExFinder("^(\\S*\\@)(\\S*\\.)(\\w{2,3})$", path);
+		//set strings for RegEx's
+		String regexDate = "(\\d{1,2}\\.){2}(?:\\d{2}){1,2}";
+		String regexPostal = "(\\ )[34][0-5](\\d{3})(\\ )";
+		String regexTel = "(\\w{7}\\ )(\\+49\\ )(\\d{1,4}\\ )(\\d{1,7})(?:\\-|\\ )(?:\\d{0,5})";
+		String regexInet = "(?:w{3}\\.|\\bWWW\\ )(\\S*\\.)(\\w{2,3})";
+		String regexMail = "(\\S*\\@)(\\S*\\.)(\\w{2,3})";
 		
-		//start search for the RegExFinder
-		System.out.println("Start listing dates found in document... \n");
+		//creating various RegExFinder
+		RegExFinder regDate = new RegExFinder(regexDate, path);
+		RegExFinder regPostal = new RegExFinder(regexPostal, path);
+		RegExFinder regTel = new RegExFinder(regexTel, path);
+		RegExFinder regInet = new RegExFinder(regexInet, path);
+		RegExFinder regMail = new RegExFinder(regexMail, path);
+		
+		//start search with the RegExFinder
+		System.out.println("Start listing dates found in document...\n");
 		regDate.search();
 		
-		/*~ System.out.println("\nStart listing postalcodes found in document... \n");
+		System.out.println("Start listing postalcodes found in document...\n");
 		regPostal.search();
 		
-		System.out.println("\nStart listing telephone numbers found in document... \n");
+		System.out.println("Start listing telephone numbers found in document...\n");
 		regTel.search();
 		
-		System.out.println("\nStart listing internet addresses found in document... \n");
+		System.out.println("Start listing internet addresses found in document...\n");
 		regInet.search();
 		
-		System.out.println("\nStart listing emails found in document... \n");
-		regMail.search(); ~*/
+		System.out.println("Start listing emails found in document...\n");
+		regMail.search();
 		
-		//write finish comments
+		//giving information about how much RegEx's were found in total
+		int overall = regDate.getOccurs() + regPostal.getOccurs() + regTel.getOccurs() + regInet.getOccurs() + regMail.getOccurs();
+		System.out.println("In the current run " + overall + " substrings were found that matched one of the given regular expressions: \n"
+				+ "Regular expression for dates was: " + regexDate + "\n"
+				+ "Regular expression for postalcodes was: " + regexPostal + "\n"
+				+ "Regular expression for telephone numbers was: " + regexTel + "\n"
+				+ "Regular expression for internet addresses was: " + regexInet + "\n"
+				+ "Regular expression for mail addresses was: " + regexMail + ".");
+		
+		//write finishing comments
 		System.out.println("\n\nEnd program...");
 		System.out.println("Finished.");
 	}
